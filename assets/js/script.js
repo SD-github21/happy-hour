08ebd521c6d1327b75bda9fd5291b942c7557661
SD-github21 <sapandonde.gmail.com>
2022-04-27 22:25:12 -0500

    revise code to include free API link to cocktail DB and added pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,66 +8,6 @@
 // ingredients list
 
 
-
-
-  // // Create getCocktailData() that feeds in cocktail name and calls the Cocktail DB API URL
-  // var getCocktailData = function(cocktail) {
-
-  //   const options = {
-  //       method: 'GET',
-  //       headers: {
-  //           'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
-  //           'X-RapidAPI-Key': '5c8db769e9msh5db48a3075f2811p17399bjsn3bbad4940064'
-  //       }
-  //   };
-    
-  //   // Format the Cocktail DB API URL to accept a cocktail name
-  //   var apiUrl = 'https://the-cocktail-db.p.rapidapi.com/search.php?s=' + cocktail;
-  
-  //   // Fetch cocktail data from the API URL
-  //   fetch(apiUrl, options)
-  //     .then(function(response) {
-  //         console.log(response);
-  //       // If request is successful, convert cocktail data from the URL into JSON and feed it into the function 
-  //       if (response.ok) {
-  //         response.json().then(function(data) {
-  //           console.log(data);
-  //           // Create elements for cocktail name, instructions, ingredients, and picture to display on webpage
-  //           // var cocktailHeaderEl = document.createElement("h3");
-  //           // cocktailHeaderEl.setAttribute("id", "cocktail-name");
-  //           // cocktailHeaderEl.textContent = ;
-  //           // cocktailContainerEl.appendChild(cocktailHeaderEl);
-
-
-  //         });
-
-  //       // Create alerts for any errors that might come up regarding the API call
-  //       } else {
-  //         alert('Error: ' + response.statusText);
-  //         // Clear out data from any previous searches
-
-  //       }
-  //     })
-  //     .catch(function(error) {
-  //       alert('Unable to connect to Cocktail DB API');
-  //     });
-  // };
-
-
-
-
-
-// Feed cocktail data into the Google Translate API 
-
-// Fetch data
-// Translation of instructions and ingredients list
-
-// Display on page
-// Dynamically generate HTML 
-
-// Create a favorites list for the cocktails (localStorage)
-
-
 var getCocktailData = function() {
 
   
@@ -116,6 +56,15 @@ var getCocktailData = function() {
       alert('Unable to connect to Cocktail DB API');
     });
 };
+
+// Feed cocktail data into the Google Translate API 
+// Fetch data
+
+// Dynamically generate HTML to display translation of how to order drink in region's native language
+
+// Create a favorites list for the cocktails (localStorage)
+// Store cocktails into an array on localStorage
+// Dynamically generate buttons with cocktail names that will persist upon closing browser or refreshing browser
 
 
 
