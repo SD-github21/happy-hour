14f512b4cf0cc53d877ff176639225a6958c8861
SD-github21 <sapandonde.gmail.com>
2022-04-30 21:27:50 -0500

    added conditionals for languages, completed translation modal, fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var cocktailFormEl = document.querySelectorAll("#cocktail-form");
 var cocktailSearchContainerEl = document.querySelector("#cocktail-search");
 var cocktailSelectBtnEl = document.querySelector("#select-btn");
 var cocktailInputEl = document.querySelector("#cocktail-options");
+var translateModalEl = document.querySelector(".lead");
+
 // Search form
 // Part 1 Cocktail to search by (1) ingredient or (2) name of cocktail **drop down box
 // Part 2 Language User to choose language ** drop down box
@@ -18,30 +20,30 @@ var searchCocktailHandler = function(event) {
 
     // Get cocktail name value from input element
 
-    var cocktailname = $("#cocktail-options").val();
+    var cocktailname = $("#cocktail-options option:selected").text();
     console.log(cocktailname);
-
+    var cocktailvalue = $("#cocktail-options").val();
+    console.log(cocktailvalue)
       
-    // Feed user's entered cocktail name into functions that will make Coctail DB & Google Translate calls 
+    // Feed user's entered cocktail name into functions that will make Cocktail DB & Google Translate calls 
     // and generate buttons
-    if (cocktailname) {
-      getCocktailData(cocktailname);
-      // getTranslateData(cocktailname);
+      getCocktailData(cocktailvalue);
+      if (cocktailname === "Black Russian" || cocktailname === "Negroni" || cocktailname === "Mimosa" ||
+      cocktailname === "Sangria" || cocktailname === "Pina Colada" || cocktailname === "Margarita") {
+      getTranslateData(cocktailname);
+      } else {
+        translateModalEl.textContent = "";
+        var translationOrderEl = document.createElement("p");
+        translationOrderEl.textContent = "I would like to order a " + cocktailname;
+        translateModalEl.appendChild(translationOrderEl);
+
+      };
       // buttonGenerator(cocktailname);
       // Push cocktail name into cocktails array and store in localStorage
       cocktails.push(cocktailname);
       console.log(cocktails);
       localStorage.setItem("cocktails", JSON.stringify(cocktails));
-      // Clear input form element 
-
-    
-     // Alert user to select a cocktail if the input element is blank 
-    } else {
-      alert('Please choose a cocktail');
-    }
   };
-
-
 
 
 // Fetch data 
@@ -173,55 +175,84 @@ var getCocktailData = function(name) {
 
 // Feed cocktail data into the Google Translate API 
 // Fetch data
-// var getTranslateData = function() {
-//   var encodedParams = new URLSearchParams();
-//   encodedParams.append("q", "I would like to order ");
-//   encodedParams.append("target", "es");
-//   encodedParams.append("source", "en");
+
+var getTranslateData = function(cocktailname) {
   
+  var encodedParams = new URLSearchParams();
 
-//   var options = {
-//     method: 'POST',
-//     headers: {
-//       'content-type': 'application/x-www-form-urlencoded',
-//       'Accept-Encoding': 'application/gzip',
-//       'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-//       'X-RapidAPI-Key': '5c8db769e9msh5db48a3075f2811p17399bjsn3bbad4940064'
-//     },
-//     body: encodedParams
-//   };
+  if (cocktailname === "Sangria" || cocktailname === "Pina Colada" || cocktailname=== "Margarita") {
+    encodedParams.append("q", "I would like to order a");
+    encodedParams.append("target", "es");
+    encodedParams.append("source", "en");
+} else if (cocktailname === "Mimosa" || cocktailname === "French 75") {
+    encodedParams.append("q", "I would like to order a ");
+    encodedParams.append("target", "fr");
+    encodedParams.append("source", "en");
+} else if (cocktailname === "Negroni") {
+    encodedParams.append("q", "I would like to order a");
+    encodedParams.append("target", "it");
+    encodedParams.append("source", "en");
+} else {
+  encodedParams.append("q", "I would like to order a");
+  encodedParams.append("target", "de");
+  encodedParams.append("source", "en");
+}; 
 
-//   fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-//     .then(function(response) {
-//       console.log(response);
-//       if (response.ok) {
-//         response.json().then(function(data) {
-//           console.log(data);
+ var options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Accept-Encoding': 'application/gzip',
+      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'ac4479e297msh2dfafef294e9270p1a1ecajsna98950f2bde4'
+    },
+    body: encodedParams
+  };
 
-//           var translation = data.data;
-//           console.log(translation);
-//           var finaltranslate = translation.translations[0].translatedText;
-//           console.log(finaltranslate);
-//           // return(finaltranslate);
+  fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+    .then(function(response) {
+      console.log(response);
+      if (response.ok) {
+        response.json().then(function(data) {
+          console.log(data);
+
+          var translation = data.data;
+          console.log(translation);
+          var finaltranslate = translation.translations[0].translatedText;
+          console.log(finaltranslate);
+          // Dynamically generate HTML to display translation of how to order drink in region's native language
+
+          var translate = finaltranslate;
+          console.log(translate);
+          translateModalEl.textContent = "";
+          var translationOrderEl = document.createElement("p");
+          translationOrderEl.textContent = translate + " " + cocktailname;
+          translateModalEl.appendChild(translationOrderEl);
 
           
-//           });
-//       // Create alerts for any errors that might come up regarding the API call
-//     } else {
-//       alert('Error: ' + response.statusText);
-//       // Clear out data from any previous searches
+          });
 
-//     }
-//   })
-//   .catch(function(error) {
-//     alert('Unable to connect to Google Translate API');
-//   });
-// };
+      
+      // Create alerts for any errors that might come up regarding the API call
+    } else {
+      alert('Error: ' + response.statusText);
+      // Clear out data from any previous searches
 
-// Dynamically generate HTML to display translation of how to order drink in region's native language
-// var translateOrderModal = function(name, finaltranslate) {
+    }
+  })
+  .catch(function(error) {
+    alert('Unable to connect to Google Translate API');
+  });
 
-// }
+
+
+  
+  
+ 
+};
+
+
+
 
 
 // Create a favorites list for the cocktails (localStorage)
